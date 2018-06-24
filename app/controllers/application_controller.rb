3186b8d1260640443fcacd010098c52d77ff86a1
Michael Kang <michaelkang85@gmail.com>
2018-06-24 21:49:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    Rack::Session::Cookie, :key => 'rack.session', :path => '/', :secret => 'some-random-string'
   end
 
   get '/' do
